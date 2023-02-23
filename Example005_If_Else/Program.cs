de222cea91ce08a1235a36aa12de71ec94f74c56
Anatoliy.Bykin <bykinlol@mail.ru>
2023-02-23 13:45:52 +0300

    Example005

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя");
+
+string username = Console.ReadLine();
+
+if (username.ToLower == "Маша")
+{
+    Console.WriteLine("Ура Ура, наконец то Маша");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
